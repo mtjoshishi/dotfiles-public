6cbc5ab8b60f00d0c6b265d827324e4a571970f2
Makoto Teramoto <mteramoto.knct@gmail.com>
2024-02-23 14:33:28 +0900

    Neovim: Load options before loading the lazy
    To suppress "You need to set 'vim.g.mapleader' **BEFORE** loading lazy"
    warning at startup, let load 'config/options.lua' before loading
    lazy.vim script.
    
    Signed-off-by: Makoto Teramoto <mteramoto.knct@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,9 @@ if vim.loader and vim.fn.has("nvim-0.9.1") == 1 then
   vim.loader.enable()
 end
 
-require("config.lazy")
+-- To suppress 'You need to set 'vim.g.mapleader' **BEFORE** loading lazy',
+-- call options at first.
 require("config.options")
+require("config.lazy")
 require("config.keymaps")
 require("config.autocmds")
