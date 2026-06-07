6e8e6b2767f70e7eec28a7a28ec1faedd9c494ef
Makoto Teramoto <mteramoto.knct@gmail.com>
2026-06-08 08:00:57 +0900

    nvim: add several plugins of ui
    Introduce the 'bufferline.nvim' and 'noice.nvim'.
    
    Signed-off-by: Makoto Teramoto <mteramoto.knct@gmail.com>

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -5,6 +5,38 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = true,
-  }
+  },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = true,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.utils.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.utils.stylize_markdown"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      })
+    end
+  },
 }
 
