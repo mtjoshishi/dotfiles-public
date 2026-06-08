d0d23fea61a26f83dcf3cd62c867d7a929a3f389
Makoto Teramoto <mteramoto.knct@gmail.com>
2026-06-09 06:59:12 +0900

    nvim: lsp: display the error/warn message onto side of specified code
    Signed-off-by: Makoto Teramoto <mteramoto.knct@gmail.com>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,10 +14,44 @@ return {
       require("lspconfig.ui.windows").default_options.border = "single"
       vim.lsp.buf.hover({ border = "single", title = "hover" })
 
+      -- Display the error and warn into popup like vscode.
+      vim.diagnostic.config({
+        virtual_text = {
+          spacing = 4,
+          prefix = "■",
+          format = function(d)
+            local src = d.source and string.format("[%s]", d.source) or ""
+            local code = d.code and string.format("(%s)", d.code) or ""
+            return string.format("%s%s %s", src, code, d.message)
+          end,
+        },
+        float = {
+          source = "always",
+          focusable = false,
+          close_events = {
+            "CursorMoved", "CursorMovedI", "BufHidden", "InsertEnter"
+          },
+        },
+        severity_sort = true,
+      })
+
+      -- When cursor holds on the code, display warn/error info on popup.
+      vim.api.nvim_create_autocmd("CursorHold", {
+        pattern = "*",
+        callback = function()
+          if vim.api.nvim_get_mode().mode:match("^i") then return end
+          local _, _ = vim.diagnostic.open_float(nil, {
+            scope = "cursor",
+          })
+        end,
+      })
+
+      vim.o.updatetime = 300
+
       -- Keymaps
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(_)
-          nmap('K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+          nmap('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
           nmap('gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
           nmap('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
           nmap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
